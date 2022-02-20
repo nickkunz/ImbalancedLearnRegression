d483590ec996bd1804fc2625b8bdda2d97988fbe
wuwenglei <wwu077@uottawa.ca>
2022-02-20 16:57:27 -0500

    Modify test file.

---
+++ ImbalancedLearnRegression/test.py
@@ -0,0 +1,55 @@
+## load libraries
+from gn import gn
+from ro import ro
+import pandas
+
+## load data
+housing = pandas.read_csv(
+    
+    ## http://jse.amstat.org/v19n3/decock.pdf
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearnRegression/master/data/housing.csv"
+)
+
+## conduct smogn
+housing_smogn = ro(
+    
+    data = housing, 
+    y = "SalePrice",
+    #replace = True,
+    #under_samp=False
+    
+)
+
+## load data
+college = pandas.read_csv(
+    
+    ## http://jse.amstat.org/v19n3/decock.pdf
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearnRegression/master/data/College.csv"
+)
+
+## conduct smogn
+college_smogn = gn(
+    
+    data = college, 
+    y = "Grad.Rate",
+    #replace = True,
+    #under_samp=False
+     
+)
+
+## load data
+diabetic = pandas.read_csv(
+    
+    ## http://jse.amstat.org/v19n3/decock.pdf
+    "https://raw.githubusercontent.com/paobranco/ImbalancedLearnRegression/master/data/diabetic_data.csv"
+)
+
+## conduct smogn
+diabetic_smogn = gn(
+    
+    data = diabetic, 
+    y = "num_lab_procedures",
+    #replace = True,
+    #under_samp=False
+     
+)
